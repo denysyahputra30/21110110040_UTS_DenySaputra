5a9473a40e7059313578bdc81c57327762d30441
unknown <denysyahputra30@gmail.com>
2022-11-19 09:29:09 +0700

    DenyS

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PASWA 2022</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <style>
+        .jarak {
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 3%;
+        }
+    </style>
+</head>
+
+<body class="vh-100" style="background-image: url(https://images.unsplash.com/photo-1630318173886-9b43a35fff9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80); background-size:cover">
+    <div class="container">
+        <div class="jarak">
+        <h1 class="text-center text-white">Selamat datang !</h1>
+        <h2 class="text-center text-white" >Halo civitas ! Yuk Daftar Paswa sekarang</h2>
+        <div class="row justify-content-around">
+            <a class="btn btn-warning text-white" href="login.php">Login Admin</a>
+            <a class="btn btn-warning text-white" href="registrasi.php">Daftar Paswa</a>
+        </div>
+        </div>
+    </div>
+</body>
+
+</html>
